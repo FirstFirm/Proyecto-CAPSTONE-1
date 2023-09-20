3431f064e4bdcc39cf9fa7df5eac94034c79c482
cristianrodrigo063 <cr.pobletem@duocuc.cl>
2023-09-20 17:30:28 -0300

    Creación App y Modelo

---
+++ TituladosDuoc/Web/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Titulado(models.Model):
+    Run = models.CharField(max_length=11, unique=True)
+    Nombres = models.CharField(max_length=50)
+    ApelidoP = models.CharField(max_length=50)
+    ApellidoM = models.CharField(max_length=50)
+    NCeluda = models.CharField(max_length=10)
+    Celular = models.CharField(max_length=10)
+    Email = models.EmailField(unique=True)
+    Contrasena = models.CharField(max_length=50)
+    Carrera = models.CharField(max_length=50)
+    Codigo = models.IntegerField(unique=True)
